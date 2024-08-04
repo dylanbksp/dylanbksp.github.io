176f20e8eba472a8b864ba54e2879b3db1e313d0
creativeusername <dylanbackspace@gmail.com>
2024-08-03 21:39:20 -0400

    Setting up GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,15 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			// fallback: '404.html'   NOTE: Add this later.
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
+};
+
+export default config;
