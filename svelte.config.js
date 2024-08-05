f1336ac849953ac8f6b237a0de916e5276e82c94
Dylan <58371510+dylanbksp@users.noreply.github.com>
2024-08-05 19:00:49 +0000

    small fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		preprocess: vitePreprocess(),
+		preprocess: vitePreprocess()
 		adapter: adapter({
 			// fallback: '404.html'   NOTE: Add this later.
 		}),
