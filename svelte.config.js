cba9ed802021e027594c9a1437204dd33c72b948
creativeusername <dylanbackspace@gmail.com>
2024-08-05 18:18:56 -0400

    Adjusting svelte.config.js base path

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 			// fallback: '404.html'   NOTE: Add this later.
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/bkspwebsite' : '',
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
