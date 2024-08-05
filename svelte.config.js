b1acb0ebc7b19385b5f4cb7e27e6b63b4aa98df7
creativeusername <dylanbackspace@gmail.com>
2024-08-05 18:48:52 -0400

    Revert "Adjusting svelte.config.js base path"
    This reverts commit cba9ed802021e027594c9a1437204dd33c72b948.

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 			// fallback: '404.html'   NOTE: Add this later.
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.NODE_ENV === 'production' ? '/bkspwebsite' : '',
 		}
 	}
 };
