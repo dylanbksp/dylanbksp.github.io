b38dca8e0e6e4fb4bab182671cc362f559e45ed7
creativeusername <dylanbackspace@gmail.com>
2024-08-10 20:32:00 -0400

    Small touch ups

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	
+	preprocess: vitePreprocess(),
+
 	kit: {
-		preprocess: vitePreprocess(),
 		adapter: adapter({
 			// fallback: '404.html'   NOTE: Add this later.
 		}),
