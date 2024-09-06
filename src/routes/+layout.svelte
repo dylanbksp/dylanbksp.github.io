4fc657d07d0526e6d4b2618e8b8b71d5191184cb
Dylan <58371510+dylanbksp@users.noreply.github.com>
2024-09-06 15:56:32 +0000

    Created the main navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import '../global.css';
+</script>
+
+<ul id="mainnav">
+    <li><a href="/">Home</a></li>
+    <li><a href="/error">Error</a></li>
+    <li><a href="/error">Error</a></li>
+    <li><a href="/error">Error</a></li>
+    <li><a href="/error">Error</a></li>
+</ul>
+
+<div id="maincontent"><slot></slot></div>
